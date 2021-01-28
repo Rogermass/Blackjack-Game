01f572871930f42a9a62fb33507c4d82754fbc4c
Rogermass <rogermas14@gmail.com>
2021-01-28 20:51:41 +0100

    Add project files.

---
+++ Casino game/Casino game.cpp
@@ -0,0 +1,44 @@
+
+#include <iostream>
+#include "Header.h"
+#include <cstdlib>
+#include <string>
+#include <vector>
+
+
+
+int balance = 1000;
+int wager; 
+int i; 
+int PlayorNot;
+//int PTotalCard; 
+
+char NextPlayA [];
+
+std::string NextPlay; 
+using std::cout; using std::cin;
+
+int main()
+{
+
+    
+    cout << "Sir this is a casino and you're Playing Blackjack!\n"; 
+    
+    void RunItAll();
+        
+        WagerAmmount();
+
+        GenerateCards();
+
+        ChooseNextMove();
+        
+
+    
+   
+    
+
+
+    
+
+}
+
---
+++ Casino game/Header.h
@@ -0,0 +1,10 @@
+int WagerAmmount();
+void GenerateCards();
+void ChooseNextMove(); 
+void Surrender();
+void Hit();
+void Stand();
+void DoubleDown();
+void Split();
+void PlayorNoPlay(); 
+void RunItAll();
